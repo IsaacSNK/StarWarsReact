a3c510784094c83383102e0c37a8f5e58af0d3a1
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2021-07-22 15:32:32 -0600

    Queries to local express

--- src/components/StarWarsPeople.js
+++ src/components/StarWarsPeople.js
@@ -17,11 +17,19 @@ export default class StarWarsPeople extends React.Component {
         });
     }
 
-    render() {
-        if (this.state.characters.length === 0) {
-            return <>No data found</>
+    async componentDidUpdate(prevProps, prevState) {
+        if (prevState.nameQuery !== this.state.nameQuery) {
+            const data = await fetchStarWarsData(this.state.nameQuery);
+            this.setState({
+                characters: data
+            });
         }
+    }
+
+    render() {
         return (
+            <>
+            <input type="text" onChange={(event) => this.inputChangeHandler(event.target.value)}></input>
             <table>
                 <thead>
                     <tr>
@@ -35,6 +43,13 @@ export default class StarWarsPeople extends React.Component {
                     {this.state.characters.map((c, index) => <StarWarsCharacter key={index} character={c} test="1"></StarWarsCharacter>)}
                 </tbody>
             </table>
+            </>
         );
+    }
+
+    inputChangeHandler(nameQuery) {
+        this.setState({
+            nameQuery
+        });
     }
 }
